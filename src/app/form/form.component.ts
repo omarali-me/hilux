cd57b8c9d10d8b3f89f057b19c55a28822cfa9b4
Kartik Upadhyay <kartikupadhyay90@gmail.com>
2020-06-17 18:23:18 +0530

    FIx: Do not pass row in response

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from '../shared/authentication.service';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
+import { isArray } from 'util';
 
 @Component({
   selector: 'app-form',
@@ -63,8 +64,9 @@ export class FormComponent implements OnInit {
   }
 
   saveData(formData : any) {
+    console.log('form Data', this.prepareJson(formData));
     let form = new FormData();
-    form.append('data', JSON.stringify(Object.assign({stepID: this.response.stepID, dataIn: formData})));
+    form.append('data', JSON.stringify(Object.assign({stepID: this.response.stepID, dataIn: this.prepareJson(formData)})));
     this.http.post('https://wfe.ajm.re/AjmanLandProperty/index.php/applications/completeStep', form)
       .subscribe((data: any)=> {
         if (data.status == 'success') {
@@ -78,5 +80,34 @@ export class FormComponent implements OnInit {
       })
     // this.authenticationService.signin();
     // this.router.navigate(['/']);
+  }
+
+  private prepareJson(formData: any) {
+    let preparedData = {}
+    const rows = Object.values(formData);
+    for(let row of rows) {
+      if (this.isEnumerator(row)) {
+        const rowData = this.prepareArrayObject(row);
+        preparedData = Object.assign({}, preparedData, rowData);
+      } else {
+        preparedData = Object.assign({}, preparedData, row);
+      }
+    }
+    return preparedData
+  }
+
+  isEnumerator(data: any) {
+    return isArray(data);
+  }
+
+  private prepareArrayObject(row: any) {
+    let nestedObj = {};
+    const keys = _.uniq(_.flatMap(row, (o) => _.keys(o)));
+
+    for(let key of keys) {
+      nestedObj[key] = _.map(row, (o) => o[key]).filter(o => o);
+    }
+
+    return nestedObj
   }
 }
