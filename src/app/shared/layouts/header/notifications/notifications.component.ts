c98d4a7087a25c539cdab1f227162a254650b9e2
Kartik Upadhyay <kartikupadhyay90@gmail.com>
2020-06-18 18:00:51 +0530

    FIx: use my task api in notifications

--- src/app/shared/layouts/header/notifications/notifications.component.ts
+++ src/app/shared/layouts/header/notifications/notifications.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldsService } from 'src/app/shared/fields.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notifications',
@@ -8,31 +9,58 @@ import { FieldsService } from 'src/app/shared/fields.service';
 })
 export class NotificationsComponent implements OnInit {
   notifications: any;
-  constructor(private fieldsService: FieldsService) { }
+
+  constructor(
+    private fieldsService: FieldsService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.loadNotifications();
   }
 
   loadNotifications() {
-    // this.fieldsService.getUrl('http://192.168.0.150:3000/notifications').subscribe(data => {
-    //   this.notifications = data;
-    // })
+    this.fieldsService.getUrl('https://wfe.ajm.re/AjmanLandProperty/index.php/applications/myTasks')
+      .subscribe(data => {
+        this.notifications = data.filter(d => d.stepDetails.status.toLowerCase() === 'pending');
+      })
 
     // TODO:: HANDLE ERROR CASE AND DON"T MAKE ANY REQUEST AFTER IT
-    // setTimeout(() => { this.loadNotifications() }, 30 * 1000);
+    setTimeout(() => { this.loadNotifications() }, 60 * 1000);
   }
 
   getStatusClass(notification: any) {
-    switch (notification.status.toLowerCase()) {
-      case 'pending':
+    switch (notification.stepDetails.status.toLowerCase()) {
+      case 'locked':
         return 'text-warning';
       case 'completed':
         return 'text-success';
-      case 'rejected':
+      case 'pending':
         return 'text-danger';
       default:
         return 'text-info';
     }
+  }
+
+  getItemText(field: any, key: any) {
+    return ((field[key] && JSON.parse(field[key])).ar || '');
+  }
+
+  getTimeAvailable(item: any) {
+    if (item.stepDetails.status == 'completed') {
+      return (item.stepDetails.latestUserIDTimeStamp || item.stepDetails.timeStampInEffect);
+    } else {
+      return item.stepDetails.timeStampInEffect
+    }
+  }
+
+  performActionPerStatus(item: any) {
+    if (item.stepDetails.status.toLowerCase() == 'pending') {
+      this.router.navigate(['/notifications', item.stepDetails.applicationWorkflowStepID])
+    }
+  }
+
+  getNotificationText(notification: any) {
+    return `${notification.stepDetails.applicationWorkflowStepID}, ${notification.applicationDetails.applicationID}`;
   }
 }
