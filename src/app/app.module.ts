98a31eaf15b40b4f03b5367945c4e886141eb3dc
Mohammed Alghoul <mg.91@live.com>
2020-10-25 14:11:02 +0100

    Build my tasks datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -189,7 +190,8 @@ import { DeveloperDetailsComponent } from './developer-profile/developer-details
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
     PrettyJsonModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot({ positionClass: 'toast-top-left'}),
+    DataTablesModule
   ],
   providers: [
     AuthenticationGuard,
