6435cc8e5178fe28e9609e5d3e9d1597653f8b76
Kartik Upadhyay <kartikupadhyay90@gmail.com>
2021-02-08 23:03:07 +0530

    Add code for uploading files

--- src/app/units-excel-upload/units-excel-upload.component.ts
+++ src/app/units-excel-upload/units-excel-upload.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { FieldsService } from '../shared/fields.service';
+
+interface SearchParams {
+  query?: string;
+  filter?: string;
+}
 
 @Component({
   selector: 'app-units-excel-upload',
@@ -6,10 +17,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./units-excel-upload.component.css']
 })
 export class UnitsExcelUploadComponent implements OnInit {
+  formData: any = {};
+  formErrors: any = {};
+  currentSearchParams: SearchParams = {};
+  paramsSubscription = new Subscription();
+  results = [];
+  developerOptions: Observable<any>;
+  projectsOptions: Observable<any>;
+  developerDataOptionsLoading = false;
+  developerSearchInput$ = new Subject<string>();
+  projectsSearchInput$ = new Subject<string>();
+  projectDataOptionsLoading = false;
+  landDataOptionsLoading = false;
+  landSearchInput$ = new Subject<string>();
+  unitsOptions: any;
+  response: any;
+  minDate:any;
+  uploadedFile: any;
 
-  constructor() { }
+  @ViewChild('controlLabel') controlLabel: ElementRef;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fieldsService: FieldsService,
+    private http: HttpClient,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  uploadExcel(formData: any) {
+    let fd = new FormData();
+    fd.append('upload', this.uploadedFile);
+
+    this.http.post(`${environment.apiHost}/AjmanLandProperty/index.php/excel/createUnitsByExcel`, fd)
+      .subscribe((data: any) => {
+        if (data.status == 'success') {
+          this.response = data.data;
+          this.resetUploadControl();
+          // this.prepareStatusOptions(this.response);
+          // this.prepareUnitNumberOptions(this.response);
+          // this.prepareMeterTotalSoldAreaOptions(this.response);
+        } else {
+          this.formErrors = data.data;
+          this.toastr.error(JSON.stringify(data.message), 'Error');
+        }
+      }, (error) => {
+        this.toastr.error('Something went Wrong', 'Error');
+        this.router.navigate(['error']);
+      });
+  }
+
+  async updateControlLabel(event: any) {
+    const files = Array.from(event.target.files);
+    this.controlLabel.nativeElement.innerText = files.map((t:any) => t.name).join(', ').slice(0, 75);
+    this.uploadedFile = files[0];
+  }
+
+  resetUploadControl() {
+    this.formErrors = {}
+    this.controlLabel.nativeElement.innerText = 'choose file';
   }
 
 }
